d402808818b53e51c674328fdaae1d6843924cad
alif33 <alifhasan332@gmail.com>
2021-10-28 13:14:08 +0600

    laravel init

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\SubCategoryController;
-use App\Models\SubCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,35 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-
-// ++++++ Category ++++++
-
-Route::group(['prefix'=>'category'], function(){
-    Route::get('/', [CategoryController::class, 'show']);
-});
-
-Route::group(['prefix'=>'category', 'as'=>'category'], function(){
-    Route::post('/create', [CategoryController::class, 'create']);
-    Route::put('/update/{id}', [CategoryController::class, 'update']);
-    Route::delete('/delete/{id}', [CategoryController::class, 'destory']);
-});
-
-
-// ++++++ Sub Category ++++++
-
-Route::group(['prefix'=>'subcategory'], function(){
-    Route::get('/', [SubCategoryController::class, 'show']);
-});
-
-Route::group(['prefix'=>'subcategory', 'as'=>'subcategory'], function(){
-    Route::post('/create', [SubCategoryController::class, 'create']);
-    Route::put('/update/{id}', [SubCategoryController::class, 'update']);
-    Route::post('/delete/{id}', [SubCategoryController::class, 'destory']);
-});
-
-
